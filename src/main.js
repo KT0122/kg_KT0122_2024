04b598646020dacb10c9a7dfeababa60638c14c5
KT0122 <khaliltavarez@gmail.com>
2021-04-27 02:23:56 -0400

    Implemented the the ability for the main.js to take in numbers from command line call and return their phonetic equivalents. Along with checks to see if the numbers are negative and to skip over those numbers if they are.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,26 @@ function phoneticNumber(number) {
     let phoneticsList = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
     let phonetics = "";
     number = '' + number;
-    for (x in number) {
+    for (let x in number) {
         phonetics += phoneticsList[parseInt(number[x])];
     }
     process.stdout.write(phonetics);
 }
 
-phoneticNumber(10);
+const args = process.argv.slice(2);
+let culledArray = [];
+
+args.forEach( function (item) {
+    if(!(item < 0)) {
+        culledArray.push(item)
+    }
+});
+
+phoneticNumber(culledArray.shift());
+
+let x = 0, count = culledArray.length;
+while (x < count && count !== 0) {
+    process.stdout.write(", ");
+    phoneticNumber(culledArray.shift());
+    x++;
+}
